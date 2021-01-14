d65f10ecb513759656ed93affda621122223e82a
Emiko Nagasawa-Pijoan <enpijoan@gmail.com>
2021-01-14 14:12:39 -0800

    commit tree 4772f37df9387f9510bcb194b10935073a470e3b

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,6 +1,19 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
+#hash = {:blake => 500, :ashley => 2, :adam => 1}
 
 def key_for_min_value(name_hash)
-
+    min_value_key = nil
+    min_value = nil
+    #if name_hash.length == 0
+       # nil
+    #else
+        name_hash.each do |word, letters|
+            if  min_value == nil || letters < min_value 
+                min_value = letters
+                min_value_key = word
+            end
+        end
+   # end
+   min_value_key
 end
